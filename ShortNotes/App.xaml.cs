c055407e31bc6a652cbda64990e3057067786a21
Monkey D Luffy <48459415+DenisBredun@users.noreply.github.com>
2024-12-07 22:42:35 +0200

    Add project files.

---
+++ ShortNotes/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ShortNotes
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ ShortNotes/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ShortNotes
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
