105a4982ad5c0d0dff61229f18c013f6855c75bd
Monkey D Luffy <48459415+DenisBredun@users.noreply.github.com>
2024-12-10 00:40:47 +0200

    Made recognition and adding text word by word, added changing the state of the button.

--- ShortNotes/MainPage.xaml.cs
+++ ShortNotes/MainPage.xaml.cs
@@ -28,46 +28,89 @@ namespace ShortNotes
         {
             if (_isRecording == false)
             {
-                var isAuthorized = await _speechToText.RequestPermissionsAsync();
+                bool continueIfEditorHasText = await ContinueIfEditorHasText();
 
-                if (isAuthorized)
+                if (continueIfEditorHasText)
                 {
-                    try
-                    {
-                        _isRecording = true;
-                        startRecordingBt.Text = "Stop recording";
-                        startRecordingBt.BackgroundColor = Color.FromRgb(122, 105, 104);
-
-                        RecognitionText = await _speechToText.ListenAsync(
-                            CultureInfo.GetCultureInfo("uk-UA"),
-                            new Progress<string>(partialText =>
-                            {
-                                RecognitionText = partialText;
-                                OnPropertyChanged(nameof(RecognitionText));
-                            }), _tokenSource.Token);
-                    }
-                    catch (Exception ex)
-                    {
-                        if (_tokenSource.IsCancellationRequested == true)
-                        {
-                            await DisplayAlert("Information", "Recording was stopped", "OK");
-                            _tokenSource = new CancellationTokenSource();
-                        }
-                        else
-                            await DisplayAlert("Error", ex.Message, "OK");
-                    }
+                    CleanEditorAndVariables();
+                    await StartRecording();
                 }
-                else
-                    await DisplayAlert("Permission Error", "No microphone access", "OK");
             }
             else
-            {
-                _isRecording = false;
-                startRecordingBt.Text = "Start recording";
-                startRecordingBt.BackgroundColor = Color.FromRgb(36, 31, 30);
+                StopRecording();
+        }
 
-                _tokenSource?.Cancel();
+        private async Task StartRecording()
+        {
+            var isAuthorized = await _speechToText.RequestPermissionsAsync();
+            string lineBeforeSilence = "";
+
+            if (isAuthorized)
+            {
+                try
+                {
+                    ChangeRecordButtonState(120, 120, 120, "Stop recording", true);
+
+                    RecognitionText = await _speechToText.ListenAsync(
+                        CultureInfo.GetCultureInfo("uk-UA"),
+                        new Progress<string>(partialText =>
+                        {
+                            if (partialText.Length < lineBeforeSilence.Length)
+                            {
+                                RecognitionText += lineBeforeSilence;
+                                lineBeforeSilence = "";
+                            }
+
+                            lineBeforeSilence += partialText.Substring(lineBeforeSilence.Length);
+
+                            OnPropertyChanged(nameof(RecognitionText));
+                        }), _tokenSource.Token);
+                }
+                catch (Exception ex)
+                {
+                    if (_tokenSource.IsCancellationRequested == true)
+                    {
+                        await DisplayAlert("Information", "Recording was stopped", "OK");
+                        RecognitionText += lineBeforeSilence;
+                        OnPropertyChanged(nameof(RecognitionText));
+                        lineBeforeSilence = "";
+                        _tokenSource = new CancellationTokenSource();
+                    }
+                    else
+                        await DisplayAlert("Error", ex.Message, "OK");
+                }
             }
+            else
+                await DisplayAlert("Permission Error", "No microphone access", "OK");
+        }
+
+        private void StopRecording()
+        {
+            ChangeRecordButtonState(0, 0, 0, "Start recording", false);
+            _tokenSource?.Cancel();
+        }
+
+        private async Task<bool> ContinueIfEditorHasText()
+        {
+            if (string.IsNullOrEmpty(fieldForOutput.Text))
+                return true;
+            else
+                return await DisplayAlert("Confirmation", "Text field contains text. "
+                    + "The start of the new record will vanish last text."
+                    + "Do you want to continue?", "Yes", "No");
+        }
+
+        private void CleanEditorAndVariables()
+        {
+            fieldForOutput.Text = "";
+            RecognitionText = "";
+        }
+
+        private void ChangeRecordButtonState(int red, int green, int blue, string Text, bool isRecordingState)
+        {
+            startRecordingBt.BackgroundColor = Color.FromRgb(red, green, blue);
+            startRecordingBt.Text = Text;
+            _isRecording = isRecordingState;
         }
     }
 }
